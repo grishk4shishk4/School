a7f229ec5ef3babc9bd0667b67f6f506edf2974d
grishk4shishk4 <gregoriymukhin@yandex.com>
2023-10-25 09:01:13 +0300

    25.20.2023 9:00

---
+++ 20.10.2023.py
@@ -0,0 +1,11 @@
+# counter = 0
+# for i in 'ЕЭ':
+#     for u in 'ЕГЭ':
+#         for y in 'ЕГЭ':
+#             for t in 'ЕГЭ':
+#                 for r in 'ЕГЭ':
+#                     counter += 1
+# print(counter)
+
+for i in range()
+    print(int(i, 3))
